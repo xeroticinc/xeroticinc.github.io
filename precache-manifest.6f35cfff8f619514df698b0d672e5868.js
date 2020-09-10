361c8301035fa5e909ab883f18e8a1da9712265c
Mahmud Abdur Rahman <clanlord.ikot@gmail.com>
2020-09-10 06:12:50 +0600

    Updates

---
+++ precache-manifest.6f35cfff8f619514df698b0d672e5868.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ff9b67ff55f81da3fed51b3a9867cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da6a6fc234252e19ea4e",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cb623e209d60a6caa064",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "da6a6fc234252e19ea4e",
+    "url": "/static/js/2.008ea86a.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.008ea86a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb623e209d60a6caa064",
+    "url": "/static/js/main.80bb1d63.chunk.js"
+  },
+  {
+    "revision": "6babc3fad25bf890ee3f",
+    "url": "/static/js/runtime-main.58b2618e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
